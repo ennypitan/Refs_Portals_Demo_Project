9872b8a0ffd1b8b4ba3cef50b5ee477a8bc6c2f9
Ennypitan Adekoya <ennypitan@gmail.com>
2024-03-17 23:55:38 +0100

    updated

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -11,7 +11,7 @@ export default function Player() {
 
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+      <h2>Welcome to my world, {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
         <input type="text" ref={playerName} />
         <button onClick={handleClick}>Set Name</button>
